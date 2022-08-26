2723d7db04688485b4661e1aff4146f13d73c9d8
Yachiryu <daniel.castrilln@misena.edu.co>
2022-08-26 15:07:35 -0500

    Commit 3

--- Candy Crush Copia/Assets/Scripts/Board.cs
+++ Candy Crush Copia/Assets/Scripts/Board.cs
@@ -6,25 +6,32 @@ public class Board : MonoBehaviour
 {
     public int alto;
     public int ancho;
+    public int borde;
     public Tile[,] board;
     public GameObject prefTile;
 
+    public GameObject [] prefPuntos;
+
     public Camera camara;
+
+    
 
     private void Start()
     {
         CrearBoard();
         OrganizarCamara();
+        //PiezaAleatoria();
+        LlenarMatrizAleatoria();
     }
 
     void CrearBoard()
     {
-        board = new Tile[alto, ancho];
+        board = new Tile[ancho, alto];
         
 
-        for (int i = 0; i < alto; i++)
+        for (int i = 0; i < ancho; i++)
         {
-            for (int j = 0; j < ancho; j++)
+            for (int j = 0; j < alto; j++)
             {
                 GameObject go = Instantiate(prefTile);
                 go.name = "Tile(" + i + ", " + j + ")";
@@ -41,9 +48,45 @@ public class Board : MonoBehaviour
 
     void OrganizarCamara()
     {
-        camara.transform.position = new Vector3(((float)alto / 2)-.5f, (((float)ancho / 2))-.5f,- 10);
-        camara.orthographicSize = ((float)ancho / 2);
+        camara.transform.position = new Vector3(((float)ancho / 2) - .5f, (((float)alto / 2)) - .5f, -10);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+        float sizeY = ((float)alto / 2f) + borde;
+        float sizeX = (((float)ancho / 2) + borde) / aspectRatio;
+
+        camara.orthographicSize = sizeY > sizeX ? sizeY: sizeX;
+
+        //camara.orthographicSize = ((float)ancho / 2);
+        //Screen.height;
+        //Screen.width;
         //camara.aspect = ((float)ancho /(float)alto);
     }
 
+    GameObject PiezaAleatoria()
+    {
+        int numeroR = Random.Range(0, prefPuntos.Length);
+        GameObject go = Instantiate(prefPuntos[numeroR]);
+        return go;
+    }
+
+    void PiezaPosicion(GamePiece gp , int x, int y )
+    {
+        gp.transform.position = new Vector3(x, y, 0f);
+    }
+
+    void LlenarMatrizAleatoria()
+    {
+        for (int i = 0; i < ancho; i++)
+        {
+            for (int j = 0; j < alto; j++)
+            {
+                GameObject go = PiezaAleatoria();
+                PiezaPosicion(go.GetComponent<GamePiece>(),i,j);
+
+                //GamePiece gamePiece = go.GetComponent<GamePiece>();
+
+                
+            }
+        }
+    }
 }
---
+++ Candy Crush Copia/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePiece : MonoBehaviour
+{
+    public int cordenadaX;
+    public int cordenadaY;
+
+    public void Coordenada(int x , int y)
+    {
+        cordenadaX = x;
+        cordenadaY = y;
+    }
+}
